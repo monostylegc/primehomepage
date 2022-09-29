cc7d6996b4f2120ba28fd7a333f7b0e2e62d2b06
unknown <monostylegc@gmail.com>
2022-09-29 17:06:58 +0900

    usemutation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+import nc from "next-connect";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = nc({
+  onError: (err, req:NextApiRequest, res :NextApiResponse, next) => {
+    console.error(err.stack);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+})
+  
+  .get((req, res) => {
+    res.send("Hello world");
+  })
+  .post((req, res) => {
+    res.json({ hello: "world" });
+  })
+  .put(async (req, res) => {
+    res.end("async/await is also supported!");
+  })
+  .patch(async (req, res) => {
+    throw new Error("Throws me around! Error can be caught and handled.");
+  });
+
+export default handler;
