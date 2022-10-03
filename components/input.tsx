d1b480be37318bdafb2aebd1bea7269a677d39da
Geechang Lee <geechanglee@Geechangui-MacBookPro.local>
2022-10-03 23:20:27 +0900

    다 못함

---
+++ components/input.tsx
@@ -0,0 +1,55 @@
+import type { UseFormRegisterReturn } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  name: string;
+  kind?: "text" | "number";
+  type: string;
+  register: UseFormRegisterReturn;
+  required: boolean;
+}
+
+export default function Input({
+  label,
+  name,
+  kind = "text",
+  register,
+  type,
+  required,
+}: InputProps) {
+  return (
+    <div>
+      <label
+        className="mb-1 block text-sm font-medium text-gray-700"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      {kind === "text" ? (
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <input
+            id={name}
+            required={required}
+            {...register}
+            type={type}
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary"
+          />
+        </div>
+      ) : null}
+      {kind === "number" ? (
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
+            <span className="text-gray-500 text-sm">$</span>
+          </div>
+          <input
+            id={name}
+            required={required}
+            {...register}
+            type={type}
+            className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary"
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
