3e39611845a9cf9d36a19e36c64f879409fd31d6
unknown <monostylegc@gmail.com>
2022-10-18 17:48:48 +0900

    s

--- pages/diseaseinfo/[id].tsx
+++ pages/diseaseinfo/[id].tsx
@@ -1,10 +1,13 @@
 import useSWR from "swr";
 import { NextPage } from "next";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
 import Layout from "@components/layout";
 import { DiseaseInfo } from "@prisma/client";
 
+
+const TextViewer = dynamic(() => import('@components/quillviewer'), { ssr: false });
 
 interface DiseaseInfoDetailResponse {
 	ok: boolean;
@@ -17,18 +20,16 @@ const DiseaseInfoDetail : NextPage = () => {
 
     return (
         <Layout>
-            <div className="max-w-xl mx-auto space-y-12 my-32 px-2">
-                <h1 className="text-3xl text-center font-bold"> title </h1>
-                <div className="bg-slate-500 w-full aspect-video rounded-[20px] " />
-                <div>
-                    <h1>안녕하세요 우리는 배치깁니다. 안녕하세요 우리는 배치깁니다.
-                    안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.
-                    안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.
-                    안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.안녕하세요 우리는 배치깁니다.
-                    </h1>
-                </div>
-                <hr />
-            </div>     
+            {
+                data ? 
+                <div className="max-w-xl mx-auto space-y-12 my-32 px-2">
+                    <h1 className="text-3xl text-center font-bold"> { data?.diseaseInfo.title } </h1>
+                    <div className="bg-slate-500 w-full aspect-video rounded-[20px] " />
+                    <TextViewer htmlStr={ data?.diseaseInfo.content } /> 
+                    <hr />
+                </div> : <div>404</div>
+            }
+                 
         </Layout>
     )
 }
